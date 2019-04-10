aed80e1b0bcb35214e1b97c173fd0ef5758743b3
IvanRivan <c17463744@mydit.ie>
2019-04-10 16:44:48 +0100

    accesor methods for colour, constructor method, tostring method

---
+++ src/ie/tudublin/Colour.java
@@ -0,0 +1,37 @@
+package ie.tudublin;
+
+import processing.data.TableRow;
+
+public class Colour
+{
+    private String colour;
+    public int r;
+    public int g;
+    public int b;
+    public int value;
+
+
+    public void setColour(String colour)
+    {
+        this.colour = colour;
+    }
+
+    public String getColour() 
+    {
+        return colour;
+    }
+
+    public String toString()
+    {
+        return colour + "\t" + r + "\t" + g + "\t" + b + "\t" + value + "\t";
+    }
+
+    public Colour(TableRow row)
+    {
+        colour = row.getString("colour");
+        r = row.getInt("r");
+        g = row.getInt("g");
+        b = row.getInt("b");
+        value = row.getInt("value");
+    }
+}
